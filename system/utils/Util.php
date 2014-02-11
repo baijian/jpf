f1ea2ecbebbfe29903332ff03b3648addf3961f4
柏健 <jian.baij@gmail.com>
2014-02-11 15:44:15 +0800

    add password verify function

--- system/utils/Util.php
+++ system/utils/Util.php
@@ -7,4 +7,16 @@ class Utils_Util {
         $b = $tmp;
         return $a + $b;
     }
+
+    /**
+     * 必须包含数字字母和特殊字符
+     * TODO 使用正则搜索函数搜索是否包含数字字母及特殊字符等,不过效率也许有所下降
+     */
+    public static function verifyPassword($password) {
+        if (preg_match('/^([[:alpha:]]|\d|[[:punct:]])*([[:punct:]]+[[:alpha:]]+\d+|[[:punct:]]+\d+[[:alpha:]]+|\d+[[:punct:]]+[[:alpha:]]+|\d+[[:alpha:]]+[[:punct:]]+|[[:alpha:]]+[[:punct:]]+\d+|[[:alpha:]]+\d+[[:punct:]]+)+([[:alpha:]]|\d|[[:punct:]])*$/', $password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
